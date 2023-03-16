12cb344ffb6b67a464439751139969d5280ab905
Antonin Lindner <54866777+karbenatek@users.noreply.github.com>
2023-03-16 22:59:08 +0100

    resonance plot

---
+++ UMECS_resonance_plt.m
@@ -0,0 +1,12 @@
+fun_resonance = @(E, m, IW ) 1./(E.^2 .* ((E - m).^2 + IW.^2./4 ));  
+m = 135;
+IW = 20;
+E = linspace(1e-3,200,1000);
+CS = fun_resonance(E, m, IW);
+plot(E, CS);
+xlabel("E [MeV]");
+ylabel ("Cross section [a.u.]")
+%dCS = diff(CS);
+%plot(dCS);
+ylim([0 0.000001])
+title(["Resonance of " num2str(m) " MeV particle with " num2str(IW) " MeV interaction width" ])
